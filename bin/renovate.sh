29f5b957278ee2383f7cc098d2b71db85f3e8d68
Steven Terwindt <git@sbbh.cloud>
2025-12-12 08:25:35 +1100

    ops(renovate): local run script

--- bin/renovate.sh
+++ bin/renovate.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 
 export LOG_LEVEL=debug
 export RENOVATE_TOKEN=${GITHUB_TOKEN:?not set}
-renovate --dry-run Subbeh/home-ops 2>&1 | tee "${HOMELAB_DIR:?not set}/.temp/renovate-test.log"
+renovate Subbeh/home-ops "$@" 2>&1 | tee "${HOMELAB_DIR:?not set}/.temp/renovate.log"
